ef8f664cd9660a8b2491b7a676d1455eaed99a84
ex0dus-0x <ex0dus@codemuch.tech>
2018-04-30 21:13:18 -0400

    Modified code

--- janitor.c
+++ janitor.c
@@ -1,35 +1,94 @@
 #include "janitor.h"
 
-static size_t *
-janitor_hash(void *ptr)
-{
+janitor_t *head, *tail;
+pthread_mutex_t global_malloc_lock;
 
+static janitor_t *
+get_free_block(size_t size)
+{   
+    /* set head block as current block */
+    janitor_t *current = head;
+    while (current) {
+        /* check if block is marked free and if ample space is provided for allocation */
+        if (current->flag && current->size >= size)
+            return current;
+        /* check next block */
+        current = current->next;
+    }
+    return NULL;
 }
 
-static void *
-smalloc(size_t size)
-{   
-    /* by calling calloc, we don't need memset */
-    void * fd = calloc(size);
-    if (!fd){
-        fprintf(stderr, "unable to allocate memory\n");
-        exit(1);
+void *
+jmalloc(size_t size)
+{
+    size_t total_size;
+    void *block;
+    janitor_t *header;
+
+    /* error-check size */
+    if (!size)
+        return NULL;
+
+    pthread_mutex_lock(&global_malloc_lock);
+
+    /* first-fit: check if there already is a block size that meets our allocation size and immediately fill it and return */
+    header = get_free_block(size);
+    if (header){
+        header->flag = 0;
+        pthread_mutex_unlock(&global_malloc_lock);
+        return (void *) (header + 1);
     }
     
-    return fd;
+    /* if not found, continue by extending the size of the heap with sbrk, extending the break to meet our allocation */
+    total_size = BLOCK_SIZE + size;
+    block = sbrk(total_size);
+    if (block == (void *) -1 ) {
+        pthread_mutex_unlock(&global_malloc_lock);
+        return NULL;
+    }
+    
+    /* set struct entries with allocation specification and mark as not free */
+    header = block;
+    header->size = size;
+    header->flag = 0;
+    header->next = NULL;
+
+    if (!head)
+        head = header;
+    if (tail)
+        tail->next = header;
+
+    tail = header;
+    pthread_mutex_unlock(&global_malloc_lock);
+    return (void *)(header - 1);
 }
 
-void 
-janitor_start(janitor_t *gc)
+void *
+jcalloc(void *block, size_t size)
 {
-    gc->nObjects = 0;
-    gc->nFrees = 0;
-    gc->maxObjects = JANITOR_INITIAL_GC;
 
 }
 
-void 
-janitor_stop(janitor_t *gc)
+void *
+jrealloc(void *block, size_t size)
 {
-    janitor_sweep(gc);
+
 }
+
+void
+free(void *block)
+{
+    janitor_t *header, *tmp;
+    void * programbreak;
+
+    if (!block)
+        return NULL;
+    
+    pthread_mutex_lock(&global_malloc_lock);
+    header = (janitor_t *) block - 1;
+    
+    programbreak = sbrk(0);
+    // TODO
+}
+
+
--- janitor.h
+++ janitor.h
@@ -6,49 +6,12 @@
 #include <string.h>
 #include <assert.h>
 
-#define JANITOR_INITIAL_GC 1
+#define BLOCK_SIZE sizeof(janitor_t)
 
-typedef u32 uint32_t;
-
-typedef struct {
-    void * bottom;
-
-    u32 nObjects;
-    u32 nFrees;
-    u32 maxObjects;
-
+typedef janitor_block {
+    size_t size;
+    struct janitor_block *next;
+    int flag;
 } janitor_t;
 
-typedef struct gc_object {
-    u32 marked;
-    size_t size;
-    void * ptr;
-
-    /* Next object, as this is a linked-list */
-    struct gc_object * next;
-
-} Object;
-
-/* optimized memory allocation */
-// static void * smalloc(size_t size);
-
-/* hashing function for pointer */
-// static size_t * janitor_hash(void *ptr);
-
-/* Execute garbage collection */
-void janitor_start(janitor_t *gc);
-void janitor_stop(janitor_t *gc);
-
-/* Memory allocation */
-void * janitor_alloc(janitor_t *gc, size_t size);
-void janitor_free(janitor_t *gc);
-
-/* Internal object manipulation */
-// static void * janitor_add();
-// static void janitor_remove();
-
-/* Mark and sweep */
-// static void * janitor_mark(janitor_t *gc);
-// static void janitor_sweep(janitor_t *gc);
-
-
+#endif
